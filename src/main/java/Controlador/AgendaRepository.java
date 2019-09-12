907fb8e85638064f7b8c5c1094ef43246a58aff0
LizbethVera <lizveraduran@gmail.com>
2019-09-12 17:26:19 -0400

    Prueba

--- src/main/java/Controlador/AgendaRepository.java
+++ src/main/java/Controlador/AgendaRepository.java
@@ -1,6 +1,8 @@
 package Controlador;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 
 interface AgendaRepository extends JpaRepository<Agenda,Long>{
 
--- src/main/java/Controlador/BaseDatos.java
+++ src/main/java/Controlador/BaseDatos.java
@@ -1,6 +1,7 @@
 package Controlador;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,11 +10,11 @@ import org.springframework.context.annotation.Configuration;
 @Slf4j
 class BaseDatos {
 
-@Bean
-    CommandLineRunner introducirBD(AgendaRepository agenda){
+    @Bean
+    CommandLineRunner introducirBD(AgendaRepository agendaRepository){
     return args -> {
-        log.info("Cargando "+ agenda.save(new Agenda("12-02-2019","15:00","Implantologia","Mario Fernandez")));
-        log.info("Cargando "+ agenda.save(new Agenda("17-02-2019","12:00","Caries","Oscar Orzolini")));
+        log.info("Cargando "+ agendaRepository.save(new Agenda("12-02-2019","15:00","Implantologia","Mario Fernandez")));
+        log.info("Cargando "+ agendaRepository.save(new Agenda("17-02-2019","12:00","Caries","Oscar Orzolini")));
         };
     }
 }
