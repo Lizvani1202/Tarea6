2449f990b931ccbb936485ca95152b4f291b487b
LizbethVera <lizveraduran@gmail.com>
2019-09-12 13:35:46 -0400

    Empezando Prueba

---
+++ src/main/java/bo/edu/ucb/software1/demoSistema/DemoSistemaApplication.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.software1.demoSistema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSistemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSistemaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bo/edu/ucb/software1/demoSistema/DemoSistemaApplicationTests.java
@@ -0,0 +1,16 @@
+package bo.edu.ucb.software1.demoSistema;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoSistemaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
