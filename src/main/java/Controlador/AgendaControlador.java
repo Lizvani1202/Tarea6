bec256040fc695de2a32e953c66ba935b43cf046
LizbethVera <lizveraduran@gmail.com>
2019-09-12 18:37:01 -0400

    Subiendo Mejorado

--- src/main/java/Controlador/AgendaControlador.java
+++ src/main/java/Controlador/AgendaControlador.java
@@ -1,10 +1,12 @@
 package Controlador;
 
 
+import org.springframework.hateoas.Resources;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import static org.springframework.hateoas.core.DummyInvocationUtils.methodOn;
@@ -13,19 +15,28 @@ import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 @RestController
 class AgendaControlador {
     private final AgendaRepository repository;
+    private final rescursosAgenda rescursosAgenda;
 
-    public AgendaControlador(AgendaRepository repository) {
+    public AgendaControlador(AgendaRepository repository, Controlador.rescursosAgenda rescursosAgenda) {
         this.repository = repository;
+        this.rescursosAgenda = rescursosAgenda;
+    }
+
+    public AgendaRepository getRepository() {
+        return repository;
+    }
+
+    public Controlador.rescursosAgenda getRescursosAgenda() {
+        return rescursosAgenda;
     }
 
     @GetMapping("/Agenda")
     Resource<Resource<Agenda>> all(){
+
         List<Resource<Agenda>> resources = repository.findAll().stream()
-                .map(agenda -> new Resource<>(agenda,
-                        linkTo(methodOn(AgendaControlador.class).one(agenda.getId())).withSelfRel(),
-                        linkTo(methodOn(AgendaControlador.class).all()).withRel("employees")))
+                .map(rescursosAgenda::toResource)
                 .collect(Collectors.toList());
-        return new Resource<>(resources,
+        return new org.springframework.hateoas.Resource<>(resources,
                 linkTo(methodOn(AgendaControlador.class).all()).withSelfRel());
 
     }
@@ -37,13 +48,11 @@ class AgendaControlador {
     }
 
     @GetMapping("/Agenda{id}")
-    Resource one(@PathVariable Long id){
+    org.springframework.hateoas.Resource<Agenda> one(@PathVariable Long id){
         Agenda agenda = repository.findById(id)
                 .orElseThrow(()->new AgendaNotFoundException((id)));
 
-        return new Resource(agenda,
-                linkTo(methodON(AgendaControlador.class).one(id)).withSelfRel(),
-                linkTo(methodON(AgendaControlador.class).all()).withRel("agenda"));
+       return rescursosAgenda.toResource(agenda);
 
     }
     @PutMapping("/Agenda/{id}")
